178b4be5acb827d4d1f49bbe9c1077712c3727ed
brendanwong <mrbrendanwong@gmail.com>
2021-01-21 17:05:40 -0800

    lab 3 changes

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -15,4 +15,31 @@ function initializePage() {
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$(".project").click(projectClick);
+	$("#testjs").click(jsButtonClick);
+}
+
+function projectClick(e){
+	console.log("Project clicked")
+	e.preventDefault();
+
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       // description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+
+       $('.project').fadeOut();
+    }
+}
+
+function jsButtonClick(e){
+	console.log("Test Javascript button clicked")
+	e.preventDefault();
+
+	$(".jumbotron h1").text("Jumbotron header changed");
+	$("#testjs").text("Please wait...");
+
+	$(".jumbotron p").toggleClass("active");
 }
